fd558062dda904f9429303b0ad3dacd422e69a63
VAIBHAVTOMAR321 <vibhudon321@gmail.com>
2022-06-01 11:35:11 +0530

    unit 3

--- UNIT_2/P12_Task01_InterfaceInJava.java
+++ UNIT_2/P12_Task01_InterfaceInJava.java
@@ -1,64 +1,69 @@
-// package Unit_02;
+package UNIT_2;
 
-// interface Animal{
-// 	public void animalSound();
-// 	public void run();
-// }
-// interface Human{
-// 	public void humanSound();
-// 	public void run1();
-// }
-// public class P12_Task01_InterfaceInJava implements Animal{
+interface Animal {
+	public void animalSound();
 
-// 	public static void main(String[] args) {
-// 		C1 obj=new C1();
-// 		obj.funcA();
-// 		obj.funcB();
-// 	}
-// 	public void animalSound() {
-// 		// TODO Auto-generated method stub
-// 	}
-// 	public void run() {
-// 		// TODO Auto-generated method stub		
-// 	}
-// }
-// class Species implements Animal,Human{
+	public void run();
+}
 
-// 	public void animalSound() {
-// 		// TODO Auto-generated method stub	
-// 	}
-// 	public void run() {
-// 		// TODO Auto-generated method stub	
-// 	}
-// 	public void humanSound() {
-// 		// TODO Auto-generated method stub		
-// 	}
-// 	public void run1() {
-// 		// TODO Auto-generated method stub		
-// 	}
-// }
+interface Human {
+	public void humanSound();
 
-// interface A1{
-// 	void funcA();
-// }
+	public void run1();
+}
 
-// interface B1 extends A1{
-// 	void funcB();
-// }
-// class C1 implements B1{
-// 	public void funcA() {
-// 		System.out.println("This is funcA from A1");
-// 	}
+public class P12_Task01_InterfaceInJava implements Animal {
 
-// 	@Override
-// 	public void funcB() {
-// 		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		C1 obj = new C1();
+		obj.funcA();
+		obj.funcB();
+	}
 
-// 	}
-// }
+	public void animalSound() {
+		// TODO Auto-generated method stub
+	}
 
-public class P12_Task01_InterfaceInJava {
-	public static void main(String args[]) {
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+}
+
+class Species implements Animal, Human {
+
+	public void animalSound() {
+		// TODO Auto-generated method stub
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+	public void humanSound() {
+		// TODO Auto-generated method stub
+	}
+
+	public void run1() {
+		// TODO Auto-generated method stub
+	}
+}
+
+interface A1 {
+	void funcA();
+}
+
+interface B1 extends A1 {
+	void funcB();
+}
+
+class C1 implements B1 {
+	public void funcA() {
 		System.out.println("This is funcA from A1");
+	}
+
+	@Override
+	public void funcB() {
+		// TODO Auto-generated method stub
+
 	}
 }
